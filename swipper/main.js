0bc20511212a9aaf23a7fa3b1378842e5635e798
Manirul Monir <64685011+Monir312@users.noreply.github.com>
2023-01-20 11:27:10 +0600

    First commit

---
+++ swipper/main.js
@@ -0,0 +1,17 @@
+/**
+   * swiper slider
+   */
+  new Swiper('.students-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
